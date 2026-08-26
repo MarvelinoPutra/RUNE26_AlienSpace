af584bd6913050c79e066aafb8429ad31b5d3bb8
Kyuuxen <147649669+MarvelinoPutra@users.noreply.github.com>
2026-08-26 17:01:45 +0700

    Ini Nambahin prefab pt1

--- Assets/Scripts/ObstacleMove.cs
+++ Assets/Scripts/ObstacleMove.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class ObstacleMove : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float _speed = 0.65f;
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //transform.position;
     }
 }
